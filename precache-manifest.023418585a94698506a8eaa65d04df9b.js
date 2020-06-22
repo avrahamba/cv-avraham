fc853274b75f1f9b0709371a48d2ff035c85846e
Avraham <b.a.avraham@gmail.com>
2020-06-22 18:08:26 +0300

    Initial dist subtree commit

--- precache-manifest.023418585a94698506a8eaa65d04df9b.js
+++ precache-manifest.023418585a94698506a8eaa65d04df9b.js
@@ -4,12 +4,16 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/cv-avraham/404.html"
   },
   {
-    "revision": "2f456d67f34f546217f9",
-    "url": "/cv-avraham/css/app.61e0d4c5.css"
+    "revision": "e58b80676d19eb7c4041",
+    "url": "/cv-avraham/css/app.ff64352a.css"
   },
   {
     "revision": "c1c92d0c804d46cb62814e7389725082",
     "url": "/cv-avraham/fonts/GreatVibes-Regular.c1c92d0c.ttf"
+  },
+  {
+    "revision": "6f82017092b3f62c335565b373a29482",
+    "url": "/cv-avraham/fonts/KronaOne-Regular.6f820170.ttf"
   },
   {
     "revision": "2d36b1a925432bae7f3c53a340868c6e",
@@ -32,6 +36,10 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/cv-avraham/img/appsus.d3b539c0.png"
   },
   {
+    "revision": "189b3a8c003979a7e98c845160e66761",
+    "url": "/cv-avraham/img/auto.189b3a8c.png"
+  },
+  {
     "revision": "eb456ac401491de3f64a26587dd7835d",
     "url": "/cv-avraham/img/avraham.eb456ac4.jpg"
   },
@@ -46,6 +54,18 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
     "revision": "9c75ca2b0c277584e827c20c0a9ac4eb",
     "url": "/cv-avraham/img/cs.9c75ca2b.png"
+  },
+  {
+    "revision": "19b50880edd17356d7168e2a9e99b93f",
+    "url": "/cv-avraham/img/download-book.19b50880.png"
+  },
+  {
+    "revision": "1ce76117b514123c24c4d7a08c00d99a",
+    "url": "/cv-avraham/img/find.1ce76117.png"
+  },
+  {
+    "revision": "9fa37f43141760654d7f36db64ca7c35",
+    "url": "/cv-avraham/img/forum.9fa37f43.png"
   },
   {
     "revision": "bf271dbd229606ea3d952f439538c39d",
@@ -72,6 +92,10 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/cv-avraham/img/misterbitcoin.e2ac1bdb.png"
   },
   {
+    "revision": "b98478f301dc1838359232dbf354f562",
+    "url": "/cv-avraham/img/molti-screen.b98478f3.png"
+  },
+  {
     "revision": "4e2485c793f3d68c551d52b637491a27",
     "url": "/cv-avraham/img/mysql.4e2485c7.png"
   },
@@ -88,6 +112,10 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/cv-avraham/img/twitter.d77e8f32.svg"
   },
   {
+    "revision": "5da706ad792c9e71dc4dd21105d8dbd8",
+    "url": "/cv-avraham/img/word plug-in.5da706ad.png"
+  },
+  {
     "revision": "88e3b1a1d0285ce8e7c808711ea8723c",
     "url": "/cv-avraham/img/wpf.88e3b1a1.jpeg"
   },
@@ -96,16 +124,16 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/cv-avraham/img/write.ab47d415.svg"
   },
   {
-    "revision": "73c5b3e6cf90aecd9c17a03c8082b6f0",
+    "revision": "1627b6157b9760b4027ff6d6091fcbf4",
     "url": "/cv-avraham/index.html"
   },
   {
-    "revision": "c3f7f83d74013e2d86a6",
-    "url": "/cv-avraham/js/about.d6557832.js"
+    "revision": "070a97243e519159b8da",
+    "url": "/cv-avraham/js/about.c4042e39.js"
   },
   {
-    "revision": "2f456d67f34f546217f9",
-    "url": "/cv-avraham/js/app.6794c423.js"
+    "revision": "e58b80676d19eb7c4041",
+    "url": "/cv-avraham/js/app.4fabc931.js"
   },
   {
     "revision": "55a524bdbffe73dcbc42",
